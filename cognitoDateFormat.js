8f6654c2f75241a14537dc690e58e346fe194640
John Griffith <john@John-MacBook-Air.local>
2022-07-29 15:17:02 -0400

    Broke specific date-format implementation into a separate method.

--- cognitoDateFormat.js
+++ cognitoDateFormat.js
@@ -1,13 +1,19 @@
-// Target format: Fri Jul 29 18:24:01 UTC 2022
-const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const getDateStrings = () => {
+    const now = new Date();
+    const cognitoDateString = getCognitoDateFormat(now);
+};
 
-const date = new Date()
-const weekday = weekdays[date.getUTCDay()].substring(0, 3);
-const month = months[date.getUTCMonth()].substring(0, 3);
-const day = date.getUTCDate().toString().padStart(2, "0");
-const hour = date.getUTCHours().toString().padStart(2, "0");
-const minute = date.getUTCMinutes().toString().padStart(2, "0");
-const second = date.getUTCSeconds().toString().padStart(2, "0");
+// Generate a date string with the following target format: Fri Jul 29 18:24:01 UTC 2022
+const getCognitoDateFormat = (date) => {
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const dateString = `${weekday} ${month} ${day} ${hour}:${minute}:${second} UTC ${date.getFullYear()}`;
+    const weekday = weekdays[date.getUTCDay()].substring(0, 3);
+    const month = months[date.getUTCMonth()].substring(0, 3);
+    const day = date.getUTCDate().toString().padStart(2, "0");
+    const hour = date.getUTCHours().toString().padStart(2, "0");
+    const minute = date.getUTCMinutes().toString().padStart(2, "0");
+    const second = date.getUTCSeconds().toString().padStart(2, "0");
+
+    return `${weekday} ${month} ${day} ${hour}:${minute}:${second} UTC ${date.getFullYear()}`;
+};
